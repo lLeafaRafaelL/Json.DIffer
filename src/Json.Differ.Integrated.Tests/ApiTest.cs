f2ad35710e80b19f67092cf9dda7d3f71fd4750b
LEAFARAFAEL-PC\Rafae <rafael.developer@live.com>
2022-12-10 17:43:53 -0300

    Implementing tests order

--- src/Json.Differ.Integrated.Tests/ApiTest.cs
+++ src/Json.Differ.Integrated.Tests/ApiTest.cs
@@ -5,23 +5,24 @@ using Microsoft.AspNetCore.Mvc.Testing;
 
 namespace Json.Differ.Integrated.Tests
 {
+    [TestCaseOrderer("Json.Differ.Integrated.Tests", "Api.Tests")]
     public class ApiTest : IClassFixture<CustomWebApplicationFactory<Program>>
     {
         private readonly WebApplicationFactory<Program> _factory;
-        private const string baseUri = "json-differ/v1/diff/";
-        private Guid _externalId = Guid.NewGuid();
+        private const string baseUri = "json-differ/v1/diff";
+        private Guid ExternalId => Guid.NewGuid();
 
         public ApiTest(CustomWebApplicationFactory<Program> factory)
         {
             _factory = factory;
         }
 
-        [Fact]
+        [Fact(DisplayName = "Upload json encoded left file"), TestPriority(1)]
         public async Task Post_UploadLeftFile_ResultShouldBeCreated()
         {
             //Arrange
             var client = _factory.CreateClient();
-            var endpoint = $"{baseUri}/{_externalId}/left";
+            var endpoint = $"{baseUri}/{ExternalId}/left";
             var request = new FileToUploadDto
             {
                 EncodedFile = "eyAiVGVzdE5hbWUiIDogIkZpbGVzIGFyZSBkaWZmZXJlbnRzIiwgIlByb3BlcnR5MiIgOiAiWFlaICJ9"
@@ -36,12 +37,32 @@ namespace Json.Differ.Integrated.Tests
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
         }
 
-        [Fact]
+        [Fact(DisplayName = "Upload json encoded right file wich the externalId is already used"), TestPriority(2)]
+        public async Task Post_UploadLeftFile_WithExternalIdAlreadyUsed_ResultShouldBeBadRequest()
+        {
+            //Arrange
+            var client = _factory.CreateClient();
+            var endpoint = $"{baseUri}/{ExternalId}/left";
+            var request = new FileToUploadDto
+            {
+                EncodedFile = "eyAiVGVzdE5hbWUiIDogIkZpbGVzIGFyZSBkaWZmZXJlbnRzIiwgIlByb3BlcnR5MiIgOiAiWFlaICJ9"
+            };
+
+            //Act
+            var response = await client.PostAsync(endpoint, request.ToHttpStringContent());
+
+            //Assert
+
+            response.EnsureSuccessStatusCode();
+            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
+        }
+
+        [Fact(DisplayName = "Upload json encoded right file"), TestPriority(3)]
         public async Task Post_UploadRightFile_ResultShouldBeCreated()
         {
             //Arrange
             var client = _factory.CreateClient();
-            var endpoint = $"{baseUri}/{_externalId}/right";
+            var endpoint = $"{baseUri}/{ExternalId}/right";
             var request = new FileToUploadDto
             {
                 EncodedFile = "eyAiVGVzdE5hbWUiIDogIkZpbGVzIGFyZSBkaWZmZXJlbnRzIiwgIlByb3BlcnR5MiIgOiAiWFlaICJ9"
@@ -56,12 +77,12 @@ namespace Json.Differ.Integrated.Tests
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
         }
 
-        [Fact]
+        [Fact(DisplayName = "Upload json encoded right file wich the externalId is already used"), TestPriority(4)]
         public async Task Post_UploadRightFile_WithExternalIdAlreadyUsed_ResultShouldBeBadRequest()
         {
             //Arrange
             var client = _factory.CreateClient();
-            var endpoint = $"{baseUri}/{_externalId}/right";
+            var endpoint = $"{baseUri}/{ExternalId}/right";
             var request = new FileToUploadDto
             {
                 EncodedFile = "eyAiVGVzdE5hbWUiIDogIkZpbGVzIGFyZSBkaWZmZXJlbnRzIiwgIlByb3BlcnR5MiIgOiAiWFlaICJ9"
@@ -76,28 +97,13 @@ namespace Json.Differ.Integrated.Tests
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
         }
 
-        [Fact]
-        public async Task Post_CompareFiles_WithExternalIdAlreadyUsed_ResultShouldBeBadRequest()
-        {
-            //Arrange
-            var client = _factory.CreateClient();
-            var endpoint = $"{baseUri}/{_externalId}";
 
-            //Act
-            var response = await client.PostAsync(endpoint, null);
-
-            //Assert
-
-            response.EnsureSuccessStatusCode();
-            response.StatusCode.Should().Be(System.Net.HttpStatusCode.BadRequest);
-        }
-
-        [Fact]
+        [Fact(DisplayName = "Compare the uploaded json files"), TestPriority(5)]
         public async Task Post_CompareFiles_ResultShouldBeCreated()
         {
             //Arrange
             var client = _factory.CreateClient();
-            var endpoint = $"{baseUri}/{_externalId}";
+            var endpoint = $"{baseUri}/{ExternalId}";
 
             //Act
             var response = await client.PostAsync(endpoint, null);
@@ -106,14 +112,14 @@ namespace Json.Differ.Integrated.Tests
 
             response.EnsureSuccessStatusCode();
             response.StatusCode.Should().Be(System.Net.HttpStatusCode.Created);
-        }   
+        }
 
-        [Fact]
+        [Fact(DisplayName = "Try to compare the uploaded json files wich i was already compared"), TestPriority(6)]
         public async Task Post_CompareFiles_AreadyCompared_ResultShouldBeOK()
         {
             //Arrange
             var client = _factory.CreateClient();
-            var endpoint = $"{baseUri}/{_externalId}";
+            var endpoint = $"{baseUri}/{ExternalId}";
 
             //Act
             var response = await client.PostAsync(endpoint, null);
